0918f1de7e29fdb3ded03457e91265b1e670640d
AnkitGupta <ankitgupta72950@gmail.com>
2025-04-01 15:18:56 +0530

    update package

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone", // Creates optimized production build
+  // If you need static export instead, use:
+};
+
+module.exports = nextConfig;
